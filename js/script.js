827427dfe081d9f846e5efa7f007563139763992
Marco Ramos <mfilipe@gmail.com>
2020-04-07 18:50:51 -0400

    count down alert

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 //"Warning! This message will self-destruct in"
 alert("Warning! This message will self-destruct in")
 
-
 // 2. Display a "3... 2... 1..." countdown using 3 alert dialog boxes
-
+alert("3");
+alert("2");
+alert("1");
 
 // 3. This statement selects the <h1> element and replaces its text with "BOOM!".
 document.querySelector("h1").textContent = "🔥BOOM!🔥";
